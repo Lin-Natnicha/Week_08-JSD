fb6aebb6a11edd6996c9a1797b40d754d80243ca
Lin-Natnicha <lin88natnicha@gmail.com>
2026-04-21 17:50:56 +0700

    Test Change JSD 12

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
   return (
     <div className="pb-80 py-10 gap-y-4 flex flex-col justify-center items-center min-h-screen bg-gray-800 text-white">
       <p className="text-purple-300">
-        Message for JSD12: {" "}
+        Message for JSD: {""}
         <span className="text-yellow-300">
           {/* question or waiting for a message */}
           {question ? question : "Waiting for a message..."}
